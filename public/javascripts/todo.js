ef390357d38588f63da1158571a11c8df28f7c6c
Mark Van Holstyn <mvanholstyn@gmail.com>
2009-10-20 16:39:58 -0400

    1. Inline Delete

--- public/javascripts/todo.js
+++ public/javascripts/todo.js
@@ -1,90 +1,90 @@
-var TodoList = Behavior.create({
-  initialize: function() {
-    this.notice                  = $$(".notice").first();
-    this.todo_list               = this.element;
-    this.new_todo_control        = this.todo_list.down("a.new_todo")
-    this.new_todo_form           = this.todo_list.down("form.new_todo");
-    this.new_todo_form_errors    = this.new_todo_form.down(".errors");
-    this.cancel_new_todo_control = this.new_todo_form.down(".cancel");
-    this.todos                   = this.todo_list.down(".todos");
-
-    this.new_todo_control.observeExclusively("click", this.showNewTodoForm.bind(this));
-    this.cancel_new_todo_control.observeExclusively("click", this.hideNewTodoForm.bind(this));
-    this.new_todo_form.observeExclusively("submit", this.createNewTodo.bind(this));
-    this.todos.select(".todo").each(this.initializeTodo.bind(this));
-
-    this.notice.hide();
-    this.new_todo_form.hide();
-  },
-  
-  initializeTodo: function(todo) {
-    var delete_control = todo.down(".delete");
-    delete_control.observeExclusively("click", this.deleteTodo.bind(this, todo, delete_control));
-  },
-  
-  showNewTodoForm: function() {
-    this.new_todo_control.hide();
-    this.new_todo_form.show();
-    this.new_todo_form.focusFirstElement();
-  },
-  
-  hideNewTodoForm: function() {
-    this.new_todo_form.hide();
-    this.new_todo_form.reset();
-    this.new_todo_form_errors.update("");
-    this.new_todo_control.show();
-  },
-  
-  createNewTodo: function() {
-    this.new_todo_form.request({
-      requestHeaders: { 'Accept': 'application/json' },
-      onSuccess: function(json) {
-        this.hideNewTodoForm();
-        this.showNotice(json.notice);
-        this.insertNewTodo(json.html);
-      }.bindAsJSONResponse(this),
-      on422: function(json) {
-        this.showErrors(json.errors);
-      }.bindAsJSONResponse(this)
-    });
-  },
-  
-  insertNewTodo: function(html) {
-    var todo = Element.fromHTML(html);
-    this.initializeTodo(todo);
-    todo.hide();
-    this.todos.insert(todo);
-    todo.appear();
-  },
-
-  deleteTodo: function(todo, delete_control) {
-    if(confirm(delete_control.getAttribute("data-confirm-message"))) {
-      delete_control.request({
-        requestHeaders: { 'Accept': 'application/json' },
-        method: 'delete',
-        onSuccess: function(json) {
-          this.showNotice(json.notice);
-        }.bindAsJSONResponse(this)
-      });
-      todo.fade();
-    }
-  },
-  
-  showNotice: function(notice) {
-    this.notice.update(notice);
-    this.notice.appear();
-    this.hideNotice.bind(this).delay(10);
-  },
-  
-  hideNotice: function() {
-    this.notice.fade();
-  },
-  
-  showErrors: function(errors) {
-    this.new_todo_form_errors.update(errors);
-  }
-});
-
-Event.addBehavior({
-  '.todo_list': TodoList
-});
+// var TodoList = Behavior.create({
+//   initialize: function() {
+//     this.notice                  = $$(".notice").first();
+//     this.todo_list               = this.element;
+//     this.new_todo_control        = this.todo_list.down("a.new_todo")
+//     this.new_todo_form           = this.todo_list.down("form.new_todo");
+//     this.new_todo_form_errors    = this.new_todo_form.down(".errors");
+//     this.cancel_new_todo_control = this.new_todo_form.down(".cancel");
+//     this.todos                   = this.todo_list.down(".todos");
+// 
+//     this.new_todo_control.observeExclusively("click", this.showNewTodoForm.bind(this));
+//     this.cancel_new_todo_control.observeExclusively("click", this.hideNewTodoForm.bind(this));
+//     this.new_todo_form.observeExclusively("submit", this.createNewTodo.bind(this));
+//     this.todos.select(".todo").each(this.initializeTodo.bind(this));
+// 
+//     this.notice.hide();
+//     this.new_todo_form.hide();
+//   },
+//   
+//   initializeTodo: function(todo) {
+//     var delete_control = todo.down(".delete");
+//     delete_control.observeExclusively("click", this.deleteTodo.bind(this, todo, delete_control));
+//   },
+//   
+//   showNewTodoForm: function() {
+//     this.new_todo_control.hide();
+//     this.new_todo_form.show();
+//     this.new_todo_form.focusFirstElement();
+//   },
+//   
+//   hideNewTodoForm: function() {
+//     this.new_todo_form.hide();
+//     this.new_todo_form.reset();
+//     this.new_todo_form_errors.update("");
+//     this.new_todo_control.show();
+//   },
+//   
+//   createNewTodo: function() {
+//     this.new_todo_form.request({
+//       requestHeaders: { 'Accept': 'application/json' },
+//       onSuccess: function(json) {
+//         this.hideNewTodoForm();
+//         this.showNotice(json.notice);
+//         this.insertNewTodo(json.html);
+//       }.bindAsJSONResponse(this),
+//       on422: function(json) {
+//         this.showErrors(json.errors);
+//       }.bindAsJSONResponse(this)
+//     });
+//   },
+//   
+//   insertNewTodo: function(html) {
+//     var todo = Element.fromHTML(html);
+//     this.initializeTodo(todo);
+//     todo.hide();
+//     this.todos.insert(todo);
+//     todo.appear();
+//   },
+// 
+//   deleteTodo: function(todo, delete_control) {
+//     if(confirm(delete_control.getAttribute("data-confirm-message"))) {
+//       delete_control.request({
+//         requestHeaders: { 'Accept': 'application/json' },
+//         method: 'delete',
+//         onSuccess: function(json) {
+//           this.showNotice(json.notice);
+//         }.bindAsJSONResponse(this)
+//       });
+//       todo.fade();
+//     }
+//   },
+//   
+//   showNotice: function(notice) {
+//     this.notice.update(notice);
+//     this.notice.appear();
+//     this.hideNotice.bind(this).delay(10);
+//   },
+//   
+//   hideNotice: function() {
+//     this.notice.fade();
+//   },
+//   
+//   showErrors: function(errors) {
+//     this.new_todo_form_errors.update(errors);
+//   }
+// });
+// 
+// Event.addBehavior({
+//   '.todo_list': TodoList
+// });
