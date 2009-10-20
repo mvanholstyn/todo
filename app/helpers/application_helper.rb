1de21d21b99b3582e8b9d3ccad0b71b423352c04
Mark Van Holstyn <mvanholstyn@gmail.com>
2009-10-20 14:06:45 -0400

    cleanup the view tempaltes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def render_as_html(*args)
+    old_format = self.template_format
+    self.template_format = :html
+    render(*args)
+  ensure
+    self.template_format = old_format
+  end
+
 end
